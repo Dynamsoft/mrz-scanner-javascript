844b0832208a100f4dd01440ee9a5160e5fc5392
felixindynamsoft <felixindynamsoft@users.noreply.github.com>
2024-11-19 20:12:12 +0000

    Deploy preview for PR 15 🛫

--- pr-preview/pr-15/js/init.js
+++ pr-preview/pr-15/js/init.js
@@ -9,9 +9,8 @@ const pDataLoad = createPendingPromise();
  */
 Dynamsoft.License.LicenseManager.initLicense("DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9");
 /**
- * You can visit https://www.dynamsoft.com/customer/license/trialLicense/?product=mrz&utm_source=docs&package=js to get your own trial license good for 30 days.
- * Note that if you downloaded this sample from Dynamsoft while logged in, the above license key may already be your own 30-day trial license.
- * For more information, see https://www.dynamsoft.com/label-recognition/programming/javascript/user-guide.html?ver=latest#specify-the-license or contact support@dynamsoft.com.
+ * You can visit https://www.dynamsoft.com/customer/license/trialLicense/?product=mrz&utm_source=github&package=js to get your own trial license good for 30 days.
+ * For more information, see https://www.dynamsoft.com/capture-vision/docs/web/programming/javascript/user-guide/mrz-scanner.html#specify-the-license or contact support@dynamsoft.com.
  * LICENSE ALERT - THE END
  */
 
