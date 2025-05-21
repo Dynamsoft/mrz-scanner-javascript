6450419421a2a94a8842f32e9289122bc13e0d1e
felixindynamsoft <indrawan.felix123@gmail.com>
2025-05-21 11:53:42 -0700

    feat: frameworks sample

---
+++ samples/frameworks/react-hooks/README.md
@@ -0,0 +1,33 @@
+# 📦 MRZ Scanner - React
+
+This sample demonstrates how to use the `MRZ Scanner` API from the [Dynamsoft MRZ Scanner SDK](https://www.dynamsoft.com/use-cases/mrz-scanner/) in a **React** app to scan an MRZ using the camera.
+
+## ✨ Features
+
+- Easy integration with pre-built UI
+- Renders MRZScanner inside a React container
+- Scans one MRZ at a time from video
+
+## 🚀 Quick Start
+
+### 1. Install Dependencies
+
+```bash
+npm install
+```
+
+### 2. Start the App
+
+```bash
+npm start
+```
+
+Then open http://localhost:3000/ to view the sample app.
+
+## 📌 Customization
+
+Please check the official [documentation](https://www.dynamsoft.com/mrz-scanner/docs/web/guides/mrz-scanner-customization.html).
+
+## 📄 Support
+
+If you have any questions, feel free to [contact Dynamsoft Support](https://www.dynamsoft.com/company/contact?utm_source=sampleReadme).
---
+++ samples/frameworks/react-hooks/src/App.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import { MRZScanner, MRZDataLabel } from "dynamsoft-mrz-scanner";
+import reactLogo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [result, setResult] = useState({
+    image: "",
+    data: "",
+  });
+  useEffect(() => {
+    // Configuration object for initializing the MRZ Scanner instance
+    const config = {
+      license: "DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9", // Replace with your Dynamsoft license key
+    };
+
+    // Create an instance of the mrzScanner with the provided configuration
+    const mrzscanner = new MRZScanner(config);
+
+    // Launch the scanner; once a mrz is detected and save the result
+    mrzscanner.launch().then((_result) => {
+      const { originalImageResult, data } = _result;
+
+      // Get captured MRZ image
+      const canvas = originalImageResult?.toCanvas();
+      const dataUrl = canvas.toDataURL("image/png");
+
+      // Format data as a string
+
+      const formattedMRZ = Object.entries(data)
+        .map(([key, value]) => {
+          return `${MRZDataLabel[key]}:\n${key === "mrzText" ? value : JSON.stringify(value)}`;
+        })
+        .join("\n\n");
+
+      setResult({ image: dataUrl, data: formattedMRZ });
+    });
+  }, []);
+  return (
+    <div className="mrz-scanner-hello-world-page">
+      <div className="mrz-scanner-title">
+        <h2 className="mrz-scanner-title-text">Hello World for React</h2>
+        <img className="mrz-scanner-title-logo" src={reactLogo} alt="logo"></img>
+      </div>
+      <div className="mrz-scanner-result-container">
+        {result?.image && <img className="result-img" src={result.image} alt="mrz-result" />}
+        {result?.data && <div className="result-data">{result.data}</div>}
+      </div>
+    </div>
+  );
+}
+
+export default App;
