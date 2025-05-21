e997b7bf861e63a3ed448fdba2f7185cb42a59de
felixindynamsoft <indrawan.felix123@gmail.com>
2025-05-21 11:53:42 -0700

    feat: frameworks sample

---
+++ samples/frameworks/angular/src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MRZScanner, MRZDataLabel } from 'dynamsoft-mrz-scanner';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  result: {
+    image: string;
+    data: string;
+  } = {
+    image: '',
+    data: '',
+  };
+
+  ngOnInit(): void {
+    // Configuration object for initializing the MRZ Scanner instance
+    const config = {
+      license: 'DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9', // Replace with your Dynamsoft license key
+    };
+
+    const mrzscanner = new MRZScanner(config);
+
+    mrzscanner.launch().then((_result) => {
+      const { originalImageResult, data } = _result;
+      const canvas = originalImageResult?.toCanvas();
+      const dataUrl = canvas.toDataURL('image/png');
+
+      const formattedMRZ = Object.entries(data)
+        .map(([key, value]) => {
+          return `${MRZDataLabel[key]}:\n${
+            key === 'mrzText' ? value : JSON.stringify(value)
+          }`;
+        })
+        .join('\n\n');
+
+      this.result = { image: dataUrl, data: formattedMRZ };
+    });
+  }
+}
---
+++ samples/frameworks/vue/src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="mrz-scanner-hello-world-page">
+    <div class="mrz-scanner-title">
+      <h2 class="mrz-scanner-title-text">Hello World for Vue</h2>
+      <img class="mrz-scanner-title-logo" src="./assets/logo.png" alt="logo" />
+    </div>
+    <div class="mrz-scanner-result-container">
+      <img v-if="result.image" class="result-img" :src="result.image" alt="mrz-result" />
+      <div v-if="result.data" class="result-data">{{ result.data }}</div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { MRZScanner, MRZDataLabel } from "dynamsoft-mrz-scanner";
+
+const result = ref({
+  image: "",
+  data: "",
+});
+
+onMounted(async () => {
+  // Configuration object for initializing the MRZ Scanner instance
+  const config = {
+    license: "DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9", // Replace with your Dynamsoft license key
+  };
+  const mrzscanner = new MRZScanner(config);
+
+  try {
+    const _result = await mrzscanner.launch();
+    const { originalImageResult, data } = _result;
+
+    const canvas = originalImageResult?.toCanvas();
+    const dataUrl = canvas.toDataURL("image/png");
+
+    const formattedMRZ = Object.entries(data)
+      .map(([key, value]) => {
+        return `${MRZDataLabel[key]}:\n${key === "mrzText" ? value : JSON.stringify(value)}`;
+      })
+      .join("\n\n");
+
+    result.value = { image: dataUrl, data: formattedMRZ };
+  } catch (error) {
+    console.error("Error scanning MRZ:", error);
+  }
+});
+</script>
+
+<style>
+.mrz-scanner-hello-world-page {
+  width: 100%;
+  height: 100%;
+  text-align: center;
+}
+
+.mrz-scanner-title {
+  height: 90px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px 0;
+}
+
+.mrz-scanner-title .mrz-scanner-title-logo {
+  margin-left: 4px;
+  width: 24px;
+  height: 24px;
+}
+
+.mrz-scanner-result-container {
+  overflow-y: auto;
+  height: 100%;
+  width: 100%;
+}
+
+.mrz-scanner-result-container .result-img {
+  width: 100%;
+}
+
+.mrz-scanner-result-container .result-data {
+  white-space: pre-line;
+  padding-bottom: 20px;
+}
+</style>
