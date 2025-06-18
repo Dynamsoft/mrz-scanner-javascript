ca0e0c77f16f24a2d07f0c7db503f91276af39da
Felix-Dynamsoft <felix.indrawan@dynamsoft.com>
2025-06-18 10:02:27 -0700

    feat: release v3.0.0 (#29)
    Co-authored-by: felixindynamsoft <indrawan.felix123@gmail.com>

--- src/build/index.ts
+++ src/build/index.ts
@@ -1,7 +1,7 @@
-import MRZScanner from "./MRZScanner";
-import MRZResultView from "./views/MRZResultView";
-import MRZScannerView from "./views/MRZScannerView";
-import { MRZDataLabel, EnumMRZData, MRZResult, MRZData, MRZDate, displayMRZDate } from "./views/utils/MRZParser";
+import MRZScanner from "../MRZScanner";
+import MRZResultView from "../views/MRZResultView";
+import MRZScannerView from "../views/MRZScannerView";
+import { MRZDataLabel, EnumMRZData, MRZResult, MRZData, MRZDate, displayMRZDate } from "../views/utils/MRZParser";
 import {
   EnumMRZScanMode,
   EnumMRZDocumentType,
@@ -12,7 +12,7 @@ import {
   ResultStatus,
   ToolbarButtonConfig,
   ToolbarButton,
-} from "./views/utils/types";
+} from "../views/utils/types";
 
 export const DynamsoftMRZScanner = {
   MRZScanner,
@@ -20,8 +20,8 @@ export const DynamsoftMRZScanner = {
   MRZResultView,
 };
 
-export type { MRZResultViewToolbarButtonsConfig, MRZResultViewConfig } from "./views/MRZResultView";
-export type { MRZScannerViewConfig } from "./views/MRZScannerView";
+export type { MRZResultViewToolbarButtonsConfig, MRZResultViewConfig } from "../views/MRZResultView";
+export type { MRZScannerViewConfig } from "../views/MRZScannerView";
 export type { MRZResult, MRZData, MRZDate, UtilizedTemplateNames, ResultStatus, ToolbarButtonConfig, ToolbarButton };
 
 export {
